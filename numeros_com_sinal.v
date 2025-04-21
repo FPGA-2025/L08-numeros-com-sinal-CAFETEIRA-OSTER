c2e673574270013308dc2f799e837f5a604b27b1
Yuri Bernardo <yuribernardosie@gmail.com>
2025-04-21 16:13:15 -0300

    Modificação em .v utilizando 'case'

--- numeros_com_sinal.v
+++ numeros_com_sinal.v
@@ -1,8 +1,9 @@
+/*
 module numeros_com_sinal(
     input signed [7:0] entrada_signed_1,
-    input signed [7:0] entrada_signed_2,
+    input signed [3:0] entrada_signed_2,
     input [7:0] entrada_unsigned_1,
-    input [7:0] entrada_unsigned_2,
+    input [3:0] entrada_unsigned_2,
     input [1:0] codigo,
     output reg [7:0] saida
 );
@@ -13,5 +14,27 @@ always @(*) begin
             (codigo == 2'b10) ? entrada_unsigned_1 + entrada_signed_1 :
             entrada_unsigned_1 + entrada_signed_2;
 end
+
+endmodule
+
+*/
+
+module numeros_com_sinal(
+    input signed [7:0] entrada_signed_1,
+    input signed [3:0] entrada_signed_2,
+    input [7:0] entrada_unsigned_1,
+    input [3:0] entrada_unsigned_2,
+    input [1:0] codigo,
+    output reg [7:0] saida
+);
+
+  always @(*) begin
+    case (codigo)
+      2'b00:  saida = entrada_signed_1 + entrada_signed_2;
+      2'b01:  saida = entrada_unsigned_1 + entrada_unsigned_2;
+      2'b10:  saida = entrada_unsigned_1 + entrada_signed_1;
+      2'b11:  saida = entrada_unsigned_1 + entrada_signed_2;
+    endcase
+  end
 
 endmodule
